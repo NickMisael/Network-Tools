a9657cb399bea2745aeb77fe527ae9a497248e2c
NickMisael <40251538+NickMisael@users.noreply.github.com>
2020-09-14 18:19:29 -0300

    Update dns.go

--- VerifyDNS/dns.go
+++ VerifyDNS/dns.go
@@ -4,10 +4,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net"
-	//"os"
+	"os"
+	"os/exec"
+	"runtime"
 	"time"
 )
+
+var so = runtime.GOOS
 
 type DNS struct {
 	Nome  string
@@ -16,7 +21,81 @@ type DNS struct {
 	Tempo float32
 }
 
-func Verificar() {
+func (d *DNS) Verificar() {
+	for j := 0; j < 10; j++ {
+		for i := 0; i < len(d); i++ {
+			in := time.Now().UnixNano()
+			Address := d[i].Addr + CONN_PORT
+			conn, err := net.Dial(CONN_TYPE, Address)
+			if err != nil {
+				fmt.Println("\tErro:", i)
+				continue
+			}
+
+			conn.Close()
+			fi := time.Now().UnixNano()
+			tF := fi - in
+			d[i].Tempo += float32(tF) / 1000000
+		}
+	}
+
+	for y := 0; y < len(d); y++ {
+		d[y].Tempo /= 10
+	}
+	for i := 0; i < len(d)-1; i++ {
+		for y := 0; y < len(d)-1; y++ {
+			if d[y].Tempo >= d[y+1].Tempo {
+				d[y].Nome, d[y+1].Nome = d[y+1].Nome, d[y].Nome
+				d[y].Addr, d[y+1].Addr = d[y+1].Addr, d[y].Addr
+				d[y].Addr2, d[y+1].Addr2 = d[y+1].Addr2, d[y].Addr2
+				d[y].Tempo, d[y+1].Tempo = d[y+1].Tempo, d[y].Tempo
+			}
+		}
+	}
+	fmt.Printf("\a")
+	fmt.Printf("\t _________________________________________\n")
+	fmt.Printf("\t|                                         |\n")
+	fmt.Printf("\t|       Host | DNS Server | Time ms       |\n")
+	fmt.Printf("\t|_________________________________________|\n")
+	fmt.Printf("\t|                                         |\n")
+	for i := 0; i < len(d); i++ {
+		fmt.Printf("\t|-----------------------------------------|\n")
+		fmt.Printf("\t| %s \n", d[i].Nome)
+		fmt.Printf("\t| P: %s \n", d[i].Addr)
+		fmt.Printf("\t| S: %s \n", d[i].Addr2)
+		fmt.Printf("\t| %.2f milisegundos \n", d[i].Tempo)
+		if i == len(d)-1 {
+			fmt.Printf("\t|_________________________________________|\n")
+		}
+	}
+}
+
+func (d *DNS) troca() {
+	if so == "windows" {
+		fmt.Println("Windows")
+	} else if so == "linux" {
+		sudo := exec.Command("sudo echo")
+		sudo.Stdout = os.Stdout
+		sudo.Run()
+		file, err := os.Create("/etc/resolv.conf")
+		if err != nil {
+			panic(err)
+		}
+		str := fmt.Sprintf("# From DHCP\nnameserver %s\nnameserver %s\n", d.Addr, d.Addr2)
+		err = ioutil.WriteFile("resolv.conf", []byte(str), 0644)
+		if err != nil {
+			panic(err)
+		}
+		cp = exec.Command("sudo cp resolv.conf /etc/resolv.conf")
+		cp.Stdout = os.Stdout
+		cp.Run()
+		rm = exec.Command("rm -rf resolv.conf")
+		rm.Stdout = os.Stdout
+		rm.Run()
+	}
+}
+
+func main() {
 	const (
 		CONN_TYPE = "tcp"
 		CONN_PORT = ":53"
@@ -95,59 +174,14 @@ func Verificar() {
 			Addr2: "84.200.70.40",
 		},
 	}
-	for j := 0; j < 10; j++ {
-		for i := 0; i < len(d); i++ {
-			in := time.Now().UnixNano()
-			Address := d[i].Addr + CONN_PORT
-			conn, err := net.Dial(CONN_TYPE, Address)
-			if err != nil {
-				fmt.Println("\tErro:", i)
-				continue
-			}
-			
-			conn.Close()
-			fi := time.Now().UnixNano()
-			tF := fi - in
-			d[i].Tempo += float32(tF) / 1000000
-		}
-	}
-
-	for y := 0; y < len(d); y++ {
-		d[y].Tempo /= 10
-	}
-	for i := 0; i < len(d)-1; i++ {
-		for y := 0; y < len(d)-1; y++ {
-			if d[y].Tempo >= d[y+1].Tempo {
-				d[y].Nome, d[y+1].Nome = d[y+1].Nome, d[y].Nome
-				d[y].Addr, d[y+1].Addr = d[y+1].Addr, d[y].Addr
-				d[y].Addr2, d[y+1].Addr2 = d[y+1].Addr2, d[y].Addr2
-				d[y].Tempo, d[y+1].Tempo = d[y+1].Tempo, d[y].Tempo
-			}
-		}
-	}
-	fmt.Printf("\a")
-	fmt.Printf("\t _________________________________________\n")
-	fmt.Printf("\t|                                         |\n")
-	fmt.Printf("\t|       Host | DNS Server | Time ms       |\n")
-	fmt.Printf("\t|_________________________________________|\n")
-	fmt.Printf("\t|                                         |\n")
-	for i := 0; i < len(d); i++ {
-		fmt.Printf("\t|-----------------------------------------|\n")
-		fmt.Printf("\t| %s \n", d[i].Nome)
-		fmt.Printf("\t| P: %s \n", d[i].Addr)
-		fmt.Printf("\t| S: %s \n", d[i].Addr2)
-		fmt.Printf("\t| %.2f milisegundos \n", d[i].Tempo)
-		if i == len(d)-1 {
-			fmt.Printf("\t|_________________________________________|\n")
-		}
-	}
-
-}
-
-func main() {
-	flagStr := flag.Bool("V",true,"Verificar a melhor opção DNS")
+	flagStr := flag.Bool("V", false, "Verificar a melhor opção DNS")
+	flagtro := flag.Bool("T", false, "Verificar a melhor opção DNS")
 	flag.Parse()
 	if *flagStr {
-		Verificar()
+		d.Verificar()
 	}
+	if *flagtro {
+		d.trocar()
+	}
+
 }
